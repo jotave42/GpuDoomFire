edd3bc13cc1722476118502374fc1076109470ae
BrLocha94 <brunolocha2@gmail.com>
2019-10-23 01:34:04 -0300

    Created update loop in matrix

--- doomfire.c
+++ doomfire.c
@@ -134,37 +134,44 @@ int main(int argc, char *argv[]){
 
 void display (void)  
 { 
-    glClear(GL_COLOR_BUFFER_BIT); 
-    glBegin(GL_POINTS); 
+    //glClear(GL_COLOR_BUFFER_BIT); 
+    //glBegin(GL_POINTS); 
     
     int elem = 40;
     int ** fireStruct =(int *) malloc(sizeof(int)*elem*elem);
     for (int i = 0; i < elem; i++)
         fireStruct[i] = (int *) malloc(sizeof(int)*elem);
     
+    while (1)
+    {
     
-    loadFireStruct(fireStruct,elem);
-    creatFireSource(fireStruct,elem);
-    calculeteFirePropagation(fireStruct,elem);
-    printMatOpenGL(fireStruct,elem);
-    //printf("=================\n");
-    //prinrtMat(fireStruct,elem);
-    //sleep(1);
+        glClear(GL_COLOR_BUFFER_BIT); 
+        glBegin(GL_POINTS); 
 
-    // iterate y up to 2*pi, i.e., 360 degree 
-    // with small increment in angle as 
-    // glVertex2i just draws a point on specified co-ordinate 
-    //for ( i = 0; i < (2 * pi); i += 0.001) 
-    //{ 
-        // let 200 is radius of circle and as, 
-        // circle is defined as x=r*cos(i) and y=r*sin(i) 
-        //x = 200 * cos(i); 
-        //y = 200 * sin(i); 
-          
-        //glVertex2i(x, y); 
-    //} 
-    glEnd(); 
-    glFlush(); 
+        loadFireStruct(fireStruct,elem);
+        creatFireSource(fireStruct,elem);
+        calculeteFirePropagation(fireStruct,elem);
+        printMatOpenGL(fireStruct,elem);
+        //printf("=================\n");
+        //prinrtMat(fireStruct,elem);
+        //sleep(1);
+
+        // iterate y up to 2*pi, i.e., 360 degree 
+        // with small increment in angle as 
+        // glVertex2i just draws a point on specified co-ordinate 
+        //for ( i = 0; i < (2 * pi); i += 0.001) 
+        //{ 
+            // let 200 is radius of circle and as, 
+            // circle is defined as x=r*cos(i) and y=r*sin(i) 
+            //x = 200 * cos(i); 
+            //y = 200 * sin(i); 
+            
+            //glVertex2i(x, y); 
+        //} 
+        glEnd(); 
+        glFlush(); 
+
+    }
 } 
 
 int main (int argc, char** argv) 
