dae1231e574a55e4efde8119f79208ceb4e34711
BrLocha94 <brunolocha2@gmail.com>
2019-10-23 00:47:28 -0300

    Initial test to opengl conversion

--- doomfire.c
+++ doomfire.c
@@ -2,7 +2,26 @@
 #include <stdlib.h>
 #include <time.h>
 #include <sys/time.h>
+#include<GL/glut.h>
 
+
+void myInit (void) 
+{ 
+    // making background color black as first  
+    // 3 arguments all are 0.0 
+    glClearColor(0.0, 0.0, 0.0, 1.0); 
+      
+    // making picture color green (in RGB mode), as middle argument is 1.0 
+    glColor3f(0.0, 1.0, 0.0); 
+      
+    // breadth of picture boundary is 1 pixel 
+    glPointSize(1.0); 
+    glMatrixMode(GL_PROJECTION);  
+    glLoadIdentity(); 
+      
+    // setting window dimension in X- and Y- direction 
+    gluOrtho2D(-780, 780, -420, 420); 
+} 
 
 void prinrtMat(int** mat,int elem){
     for (int i = 0; i < elem; i++){
@@ -15,6 +34,18 @@ void prinrtMat(int** mat,int elem){
                 printf("%d ",num);
         }
         printf("]\n");
+    }
+}
+
+void printMatOpenGL(int** mat,int elem){
+    for (int i = 0; i < elem; i++){
+        for (int j = 0; j < elem; j++){
+            int num =mat[i][j];
+            if(num<10)
+                //printf("0%d ",num);
+            else
+                glVertex2i(i, j);
+        }
     }
 }
 
@@ -83,7 +114,7 @@ int loadFireStruct(int** mat,int elem){
     
     return 0;
 }
-
+/*
 int main(int argc, char *argv[]){
     int elem = 40;
     int ** fireStruct =(int *) malloc(sizeof(int)*elem*elem);
@@ -100,3 +131,56 @@ int main(int argc, char *argv[]){
     }
     return 0;
 }
+*/
+
+void display (void)  
+{ 
+    glClear(GL_COLOR_BUFFER_BIT); 
+    glBegin(GL_POINTS); 
+    
+    int elem = 40;
+    int ** fireStruct =(int *) malloc(sizeof(int)*elem*elem);
+    for (int i = 0; i < elem; i++)
+        fireStruct[i] = (int *) malloc(sizeof(int)*elem);
+    
+    
+    loadFireStruct(fireStruct,elem);
+    creatFireSource(fireStruct,elem);
+    calculeteFirePropagation(fireStruct,elem);
+    printMatOpenGL(fireStruct,elem)
+    //printf("=================\n");
+    //prinrtMat(fireStruct,elem);
+    //sleep(1);
+
+    // iterate y up to 2*pi, i.e., 360 degree 
+    // with small increment in angle as 
+    // glVertex2i just draws a point on specified co-ordinate 
+    //for ( i = 0; i < (2 * pi); i += 0.001) 
+    //{ 
+        // let 200 is radius of circle and as, 
+        // circle is defined as x=r*cos(i) and y=r*sin(i) 
+        //x = 200 * cos(i); 
+        //y = 200 * sin(i); 
+          
+        //glVertex2i(x, y); 
+    //} 
+    glEnd(); 
+    glFlush(); 
+} 
+
+int main (int argc, char** argv) 
+{ 
+    glutInit(&argc, argv); 
+    glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB); 
+      
+    // giving window size in X- and Y- direction 
+    glutInitWindowSize(1366, 768); 
+    glutInitWindowPosition(0, 0); 
+      
+    // Giving name to window 
+    glutCreateWindow("Doom Fire 2d"); 
+    myInit(); 
+      
+    glutDisplayFunc(display); 
+    glutMainLoop(); 
+} 
