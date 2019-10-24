ec03685ccc85dc99b1b8b8182405dc19031a0792
jotave42 <jvictorcostamelo@gmail.com>
2019-10-24 02:39:33 -0300

    bugfix

--- doomfire.cu
+++ doomfire.cu
@@ -11,7 +11,7 @@ inline cudaError_t checkCuda(cudaError_t result)
 {
   if (result != cudaSuccess) {
     fprintf(stderr, "CUDA Runtime Error: %s\n", cudaGetErrorString(result));
-    assert(result == cudaSuccess);
+    //assert(result == cudaSuccess);
   }
   return result;
 }
